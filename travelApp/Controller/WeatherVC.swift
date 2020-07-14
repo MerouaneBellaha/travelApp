cb9636fbc84b031bc21c79e80dd58dd4b4ef8980
Merouane Bellaha <m.bellaha@gmail.com>
2020-07-14 15:27:12 +0200

    Navigation + tabBar set up

--- travelApp/Controller/WeatherVC.swift
+++ travelApp/Controller/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  travelApp
 //
 //  Created by Merouane Bellaha on 14/07/2020.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
