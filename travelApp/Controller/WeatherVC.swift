ea4ee830545c6117288632e249df2e18f45c3227
Merouane Bellaha <m.bellaha@gmail.com>
2020-07-20 12:34:53 +0200

    fixed ui bug / networking bug

--- travelApp/Controller/WeatherVC.swift
+++ travelApp/Controller/WeatherVC.swift
@@ -1,66 +1,117 @@
-//
-//  WeatherVC.swift
-//  travelApp
-//
-//  Created by Merouane Bellaha on 14/07/2020.
-//  Copyright © 2020 Merouane Bellaha. All rights reserved.
-//
+    //
+    //  WeatherVC.swift
+    //  travelApp
+    //
+    //  Created by Merouane Bellaha on 14/07/2020.
+    //  Copyright © 2020 Merouane Bellaha. All rights reserved.
+    //
 
-import UIKit
+    import UIKit
+    import CoreLocation
 
-class WeatherVC: UIViewController {
+    class WeatherVC: UIViewController {
 
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet var cityLabels: [UILabel]!
-    @IBOutlet var conditionLabels: [UILabel]!
-    @IBOutlet var temperaturesLabels: [UILabel]!
-    @IBOutlet var weatherIcons: [UIImageView]!
-    
-    private var httpClient = HTTPClient()
-    private var weatherData: WeatherModel!
-    private var defaults = UserDefaults.standard
+        // MARK: - IBOutlet properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        searchBar.delegate = self
+        @IBOutlet weak var searchBar: UISearchBar!
+        @IBOutlet var cityLabels: [UILabel]!
+        @IBOutlet var conditionLabels: [UILabel]!
+        @IBOutlet var temperaturesLabels: [UILabel]!
+        @IBOutlet var weatherIcons: [UIImageView]!
+        @IBOutlet weak var overlay: UIView!
 
-        setUpKeyboard()
-    }
+        // MARK: - Properties
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let userCity = defaults.string(forKey: "city") ?? "New york"
-        httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [K.query + userCity]) { self.manageResult(with: $0, forUserCity: true) }
-    }
+        private let locationManager = CLLocationManager()
+        private var httpClient = HTTPClient()
+        private var weatherData: WeatherModel!
+        private var defaults = UserDefaults.standard
 
-    private func manageResult(with result: Result<WeatherData, RequestError>, forUserCity: Bool = false) {
-        switch result {
-        case .failure(let error):
-            DispatchQueue.main.async {
-                var message: String {
-                    if error == .incorrectResponse {
-                        return forUserCity ? "Ville inconnu, veuillez verifier la ville entré dans settings" : "Impossible de trouver cette ville, vérifier l'orthographe"
-                    } else { return error.description }
-                }
-                self.setAlertVc(with: message)
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+
+            searchBar.delegate = self
+
+            setUpKeyboard()
+        }
+
+        // MARK: - ViewLifeCycle
+
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+
+            let userCity = defaults.string(forKey: "city") ?? "New york"
+            self.httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [K.query + userCity]) { self.manageResult(with: $0, forUserCity: true) }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                self.getLocationPressed()
             }
-        case .success(let weatherData):
-            DispatchQueue.main.async {
-                var index: Int { forUserCity ? 1 : 0 }
-                self.weatherData = WeatherModel(weatherData: weatherData)
-                self.cityLabels[index].text = self.weatherData.cityName
-                self.conditionLabels[index].text = self.weatherData.description
-                self.temperaturesLabels[index].text = self.weatherData.temperatureString
-                self.weatherIcons[index].image = UIImage(named: self.weatherData.conditionName)
+        }
+
+        // MARK: - IBAction methods
+
+        @IBAction func getLocationPressed() {
+            self.overlay.isHidden = false
+            locationManager.requestLocation()
+        }
+
+        @IBAction func searchPressed() {
+            guard let city = searchBar.text else { return }
+            httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [K.query + city]) { self.manageResult(with: $0) }
+        }
+
+        // MARK: - Methods
+
+        private func manageResult(with result: Result<WeatherData, RequestError>, forUserCity: Bool = false) {
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    var message: String {
+                        if error == .incorrectResponse {
+                            return forUserCity ? "Ville inconnu, veuillez verifier la ville entré dans settings" : "Impossible de trouver cette ville, vérifier l'orthographe"
+                        } else { return error.description }
+                    }
+                    self.overlay.isHidden = true
+                    self.setAlertVc(with: message)
+                }
+            case .success(let weatherData):
+                DispatchQueue.main.async {
+                    var index: Int { forUserCity ? 1 : 0 }
+                    self.weatherData = WeatherModel(weatherData: weatherData)
+                    self.weatherIcons[index].image = UIImage(named: self.weatherData.conditionName)
+                    self.cityLabels[index].text = self.weatherData.cityName
+                    self.temperaturesLabels[index].text = self.weatherData.temperatureString
+                    self.conditionLabels[index].text = self.weatherData.description
+                    if index == 0 { self.overlay.isHidden = true }
+                }
             }
         }
     }
-}
 
-extension WeatherVC: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        view.endEditing(true)
-        guard let city = searchBar.text else { return }
-        httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [K.query + city]) { self.manageResult(with: $0) }
+    // MARK: - UISearchBarDelegate
+
+    extension WeatherVC: UISearchBarDelegate {
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            view.endEditing(true)
+            guard let city = searchBar.text else { return }
+            httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [K.query + city]) { self.manageResult(with: $0) }
+        }
     }
-}
+
+    // MARK: - CLLocationManagerDelegate
+
+    extension WeatherVC: CLLocationManagerDelegate {
+
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let currentLocation = locations.last else { return }
+            locationManager.stopUpdatingLocation()
+            let currentLocationLon = currentLocation.coordinate.longitude
+            let currentLocationLat = currentLocation.coordinate.latitude
+            httpClient.request(baseUrl: (K.baseURLweather + K.weatherAPI), parameters: [(K.queryLat + String(currentLocationLat)), (K.queryLon + String(currentLocationLon))]) { self.manageResult(with: $0) }
+        }
+
+        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print(error)
+        }
+    }
--- travelApp/Model/WeatherModel.swift
+++ travelApp/Model/WeatherModel.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class WeatherModel {
+struct WeatherModel {
 
     let weatherData: WeatherData!
     var conditionId: Int?
