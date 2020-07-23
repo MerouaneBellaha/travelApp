1a6ce4cb026206603937576bfbcdb0462cc0fa56
Merouane Bellaha <m.bellaha@gmail.com>
2020-07-23 20:43:36 +0200

    added appIcon

---
+++ travelAppTests/RateManagerTest.swift
@@ -0,0 +1,26 @@
+//
+//  RateManagerTest.swift
+//  travelAppTests
+//
+//  Created by Merouane Bellaha on 23/07/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//
+
+import XCTest
+@testable import travelApp
+
+class RateManagerTest: XCTestCase {
+
+    var rateManager: RateManager!
+
+    override func setUp() {
+        rateManager = RateManager()
+    }
+
+    override func tearDown() {
+        rateManager = nil
+    }
+
+    
+
+}
