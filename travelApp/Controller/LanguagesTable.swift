b9e2d0066505f82ef75ebaa996c536a1f15dabd7
Merouane Bellaha <m.bellaha@gmail.com>
2020-07-26 14:21:11 +0200

    added LanguageTable mark

--- travelApp/Controller/LanguagesTable.swift
+++ travelApp/Controller/LanguagesTable.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class LanguagesTable: UITableViewController {
 
+    // MARK: - IBOutlet properties
+
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var noLanguageLabel: UILabel!
+
+    // MARK: - Properties
     
     var senderTag: Int!
     var languages: [Language] = []
+    var diplayedLanguages: [Language] { setDisplayedLanguages() }
+    var chosenLanguage: String = ""
+    var senderVC: UIViewController!
 
-    var diplayedLanguages: [Language] {
+    // MARK: - ViewLifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+        hideKeyboardWhenTappedAround()
+    }
+
+    // MARK: - Methods
+
+    private func setDisplayedLanguages() -> [Language] {
         if searchBar.text?.isEmpty == true {
             return languages
         } else {
@@ -25,14 +42,7 @@ class LanguagesTable: UITableViewController {
         }
     }
 
-    var chosenLanguage: String = ""
-    var senderVC: UIViewController!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        searchBar.delegate = self
-        hideKeyboardWhenTappedAround()
-    }
+    // MARK: - UITableViewDataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         noLanguageLabel.text = diplayedLanguages.isEmpty ? "no language matching" : .none
@@ -45,6 +55,8 @@ class LanguagesTable: UITableViewController {
         return cell
     }
 
+    // MARK: - UITableViewDelegate
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         chosenLanguage = diplayedLanguages[indexPath.row].name
         (senderVC as? TranslatorVC)?.languageLabels[senderTag].text = chosenLanguage
@@ -52,6 +64,8 @@ class LanguagesTable: UITableViewController {
         navigationController?.popViewController(animated: true)
     }
 }
+
+    // MARK: - UISearchBarDelegate
 
 extension LanguagesTable: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
